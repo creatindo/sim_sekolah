44bde2b26658b90dd6e83255d420ecd5ba4d767c
bulyan <boolean76@gmail.com>
2016-07-29 14:33:25 +0700

    db new

--- application/views/menu_list.php
+++ application/views/menu_list.php
@@ -5,7 +5,7 @@
     <div class='col-md-12'>
       <div class='portlet light portlet-fit portlet-datatable bordered'>
         <div class='portlet-title'>
-            <div class="caption">
+            <div class="caption">  
                 <i class="icon-settings font-dark"></i>
                 <span class="caption-subject font-dark sbold uppercase">MENU LIST </span>
             </div>
